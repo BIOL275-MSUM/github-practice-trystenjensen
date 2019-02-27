3436dae75acac809bcec47ac7e70dd7f623fb6eb
Chris Merkord <chrismerkord@gmail.com>
2019-02-27 13:09:57 -0600

    Add files via upload

---
+++ beak_length.R
@@ -0,0 +1,41 @@
+
+# load packages -----------------------------------------------------------
+
+
+# read data ---------------------------------------------------------------
+
+# read the finches data
+
+# print the finches tibble in the console
+
+# take a quick look at all the variables in the dataset
+
+
+# histogram ---------------------------------------------------------------
+
+# histogram of beak length, grouped by survival, with labels
+
+
+# summarize ---------------------------------------------------------------
+
+# summarize the dataset by outcome (survived vs. died)
+
+# print the results in the console
+
+
+# bar chart ---------------------------------------------------------------
+
+# bar chart of mean beak lengths
+
+
+# t-test ------------------------------------------------------------------
+
+# get a vector of beak lengths for birds that died
+
+# print the new object in the console... it is a vector
+
+# get a vector of beak lengths for birds that survived
+
+# print the results in the console
+
+# perform a two-sample t-test assuming unequal variances
